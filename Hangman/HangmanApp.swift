4a83ae7bcd9ebbbce53e49a3fa8bbaf698830194
Harisha <harimiru@gmail.com>
2024-02-08 12:58:02 -0500

    Initial Commit

---
+++ Hangman/HangmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangmanApp.swift
+//  Hangman
+//
+//  Created by Harisha Mirukula on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct HangmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
